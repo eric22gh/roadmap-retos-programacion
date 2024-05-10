927ca8d3fcc69e050fdbc0a9f12b9b41d08a4a5a
bor <ferali.eh@gmail.com>
2024-05-10 14:46:32 -0600

    #00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/eric22gh.py
@@ -0,0 +1,39 @@
+# https://www.python.org
+
+# Comentario en una linea y sus formas
+
+"""
+comentario en
+varias
+lineas
+
+"""
+
+
+'''
+comentario varias lines
+
+'''
+# fin
+
+my_var = "eric"
+my_ark = "edwards"
+
+MY_CONSTANT = "mi constante" # es una variable que no cambia mientras se ejecuta el programa
+# ejemplo: el valor de pi: 3.14, puede ser una variable constante.
+MY_CONSTANT = 3.14 
+
+
+my_int :int  = 1 # integrer
+
+my_float :float = 1,2,-5 # float
+
+my_bool : bool = True # booleano
+
+my_string :str = "Texto" # string 
+
+print("hola, Python")
+print(type(my_bool))
+print(type(my_int))
+print(type(my_float))
+print(type(my_string))
